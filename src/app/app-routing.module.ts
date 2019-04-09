6fbcd2401decb45e032ed68f4a6c433a5a67b855
Molly Hopper <mollyhopper95@gmail.com>
2019-04-09 13:31:12 -0400

    routing to different components works (except for '/trainers/<id>' but i'm pretty sure that's because we don't have a user model so it doesn't understand the <id>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+// import { AppComponent } from './app.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { LogoutComponent } from './components/logout/logout.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
+import { UserprofileComponent } from './components/userprofile/userprofile.component';
+import { CreateTeamComponent } from './components/create-team/create-team.component';
+import { ViewTeamComponent } from './components/view-team/view-team.component';
 
 const routes: Routes = [
-  
+  { path: '', component: HomepageComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'logout', component: LogoutComponent },
+  { path: 'pokemon', component: PokemonListComponent },
+  { path: 'trainers/<id>', component: UserprofileComponent },
+  { path: 'trainers/me', component: UserprofileComponent },
+  { path: 'team/new', component: CreateTeamComponent },
+  { path: 'team', component: ViewTeamComponent },
 ];
 
 @NgModule({
