343c42b99861d97b84422ef53e345fb4dae5c6f8
Molly Hopper <mollyhopper95@gmail.com>
2019-04-19 14:56:51 -0400

    edited homepage, started binding create and edit team components

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -18,27 +18,16 @@ export class AuthenticationService {
   constructor(private _http: HttpClient, private _router: Router) { }
 
   register(regUserData: User) {
-<<<<<<< HEAD
-    return this._http.post(`${Api_Url}/users/register`, regUserData);
-=======
     return this._http.post(`${Api_Url}/register`, regUserData);
->>>>>>> 51376d03024951d3c4f422455fb566e625fd08a6
   }
 
   login(loginInfo) {
     const str = 
       `grant-type=password&username=${encodeURI(loginInfo.email)}&password=${encodeURI(loginInfo.password)}`
     
-<<<<<<< HEAD
-    return this._http.post(`${Api_Url}/users/login`, str).subscribe( (token: Token) => {
-      console.log(token);
-      this.userInfo = token;
-      localStorage.setItem('id_token', token.access_token);
-=======
     return this._http.post(`${Api_Url}/token`, str).subscribe( (token: any) => {
       // this.userInfo = token;
       localStorage.setItem('id_token', token.token);
->>>>>>> 51376d03024951d3c4f422455fb566e625fd08a6
       this.isLoggedIn.next(true);
       this.user_id.next(token.user_id);
       this._router.navigate(['/']);
@@ -48,11 +37,7 @@ export class AuthenticationService {
   currentUser(): Observable<Object> {
     if (!localStorage.getItem('id_token')) { return new Observable(observer => observer.next(false)); }
 
-<<<<<<< HEAD
-    return this._http.get(`${Api_Url}/Account/UserInfo`, { headers: this.setHeader() });
-=======
     return this._http.get(`${Api_Url}/trainers/me`, { headers: this.setHeader() });
->>>>>>> 51376d03024951d3c4f422455fb566e625fd08a6
   }
 
   logout() {
@@ -61,19 +46,11 @@ export class AuthenticationService {
 
     const authHeader = new HttpHeaders().set('api-token', `${localStorage.getItem('id_token')}`);
 
-<<<<<<< HEAD
-    this._http.post('${Api_Url}/Account/Logout', { headers: authHeader} );
-=======
     this._http.post(`${Api_Url}/logout`, { headers: authHeader} );
->>>>>>> 51376d03024951d3c4f422455fb566e625fd08a6
     this._router.navigate(['/login']);
   }
 
   private setHeader(): HttpHeaders {
-<<<<<<< HEAD
-    return new HttpHeaders().set('api-key', `Bearer ${localStorage.getItem('id_token')}`);
-=======
     return new HttpHeaders().set('api-token', `${localStorage.getItem('id_token')}`);
->>>>>>> 51376d03024951d3c4f422455fb566e625fd08a6
   }
 }
