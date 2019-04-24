9bda412bc56b92a96df6fb3033df70f615de0146
PRMcgill <mcgill.patrick2192@gmail.com>
2019-04-24 17:13:08 -0400

    commenting out more stuff

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,9 +11,9 @@ import { CreateTeamComponent } from './components/create-team/create-team.compon
 import { ViewTeamComponent } from './components/view-team/view-team.component';
 import { ViewAllUsersComponent } from './components/view-all-users/view-all-users.component';
 import { ViewAllTeamsComponent } from './components/view-all-teams/view-all-teams.component';
-import { EditTeamComponent } from './components/edit-team/edit-team.component';
+// import { EditTeamComponent } from './components/edit-team/edit-team.component';
 import { EditUserComponent } from './components/edit-user/edit-user.component';
-import { ViewUserTeamsComponent } from './components/view-user-teams/view-user-teams.component';
+// import { ViewUserTeamsComponent } from './components/view-user-teams/view-user-teams.component';
 
 
 const routes: Routes = [
@@ -27,10 +27,10 @@ const routes: Routes = [
   { path: 'trainers/me', component: UserprofileComponent },
   { path: 'trainers/me/edit', component: EditUserComponent},
   { path: 'team/view/:teamname', component: ViewTeamComponent },
-  { path: 'team/view', component: ViewUserTeamsComponent },
+  // { path: 'team/view', component: ViewUserTeamsComponent },
   { path: 'team/new', component: CreateTeamComponent },
   { path: 'team', component: ViewAllTeamsComponent },
-  { path: 'team/edit', component: EditTeamComponent },
+  // { path: 'team/edit', component: EditTeamComponent },
 ];
 
 @NgModule({
